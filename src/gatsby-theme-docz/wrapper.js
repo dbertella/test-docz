e19d78e64e9c03564ca151c433efc6934d95d57b
Daniele Bertella <daniele.bertella@gmail.com>
2020-12-02 00:24:14 +0000

    Wrapper to js

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,15 +1,14 @@
-import React, { memo, ReactNode } from "react";
+import React from "react";
 import { ThemeProvider } from "theme-ui";
-// @ts-ignore this import is an alias and works only inside docz
-import { theme } from "@/theme";
+import theme from "@/theme";
 import { useConfig } from "docz";
 import { merge } from "lodash/fp";
 
-const Theme = memo(({ children }: { children: ReactNode }) => {
+const Theme = ({ children }) => {
   const { themeConfig } = useConfig();
   return (
     <ThemeProvider theme={merge(themeConfig, theme)}>{children}</ThemeProvider>
   );
-});
+};
 
 export default Theme;
